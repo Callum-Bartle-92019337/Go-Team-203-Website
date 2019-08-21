a2ef1e3d562ce38eeb5642446cbbba2a8c6cad9a
Cory <Cory@LAPTOP-3E9K8QI9>
2019-08-22 09:47:40 +1200

    soryy if i fuck us up opps

--- Start_Git_Clone.bat
+++ Start_Git_Clone.bat
@@ -5,7 +5,7 @@ setlocal
 SET /P AREYOUSURE=Are you sure you want to clone the repo (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
-git clone https://github.com/Callum-Bartle-92019337/Go-Team-203-Website.git .
+git clone https://github.com/Callum-Bartle-92019337/Go-Team-203-Website.git 
 pause
 
 :END
